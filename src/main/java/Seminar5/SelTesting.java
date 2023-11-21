1b4ec8a07c8286b553e7c3608dc8acd12d74d51e
kirilltimofeev <sushi-maker@yandex.ru>
2023-11-21 19:47:44 +0300

    HW Seminar 5 done2

--- src/main/java/Seminar5/SelTesting.java
+++
@@ -1,9 +0,0 @@
-package Seminar5;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class SelTesting {
-}
